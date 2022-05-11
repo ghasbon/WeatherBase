fad1060fd53075e018c981aa7ac8f152e266d901
luis hasbon <lhaxre89@gmail.com>
2022-05-11 17:13:15 +0200

    Add Fuel http lib and set basic get request for weather data on dark sky api

--- app/src/main/java/com/example/weatherbase/MainViewModel.kt
+++ app/src/main/java/com/example/weatherbase/MainViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.weatherbase
 
 import androidx.lifecycle.ViewModel
+import com.github.kittinunf.fuel.httpGet
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -14,8 +15,15 @@ class MainViewModel : ViewModel() {
     private val mutableState = MutableStateFlow(WeatherUiState())
 
     suspend fun onPostalCodeInput(code: String) = withContext(Dispatchers.IO) {
-        val testWeather = WeatherUiState("Rainy", "28", "30%")
-        mutableState.value = testWeather
+        "https://dark-sky.p.rapidapi.com/40.461564,-3.636349"
+            .httpGet()
+            .header(
+                "X-RapidAPI-Host" to "dark-sky.p.rapidapi.com",
+                "X-RapidAPI-Key" to "718390cf80mshbaae151ab7aa8acp1df702jsnac8cc8f5308b"
+            )
+            .responseString {_, _, result ->
+                mutableState.value = WeatherUiState(result.get(), "", "")
+            }
     }
 }
 
