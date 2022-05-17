9111dcca3d3fd70c3e842c9bad60858a5ad533ff
luis hasbon <lhaxre89@gmail.com>
2022-05-17 19:03:56 +0200

    Add min SDK 24, google play service location api, fetch device location and request weather data on vm

--- app/src/main/java/com/example/weatherbase/MainViewModel.kt
+++ app/src/main/java/com/example/weatherbase/MainViewModel.kt
@@ -25,6 +25,18 @@ class MainViewModel : ViewModel() {
                 mutableState.value = WeatherUiState(result.get(), "", "")
             }
     }
+
+    suspend fun onGpsLocationOk(latitude: Double, longitude: Double) {
+        "https://dark-sky.p.rapidapi.com/${latitude},${longitude}"
+            .httpGet()
+            .header(
+                "X-RapidAPI-Host" to "dark-sky.p.rapidapi.com",
+                "X-RapidAPI-Key" to "718390cf80mshbaae151ab7aa8acp1df702jsnac8cc8f5308b"
+            )
+            .responseString {_, _, result ->
+                mutableState.value = WeatherUiState(result.get(), "", "")
+            }
+    }
 }
 
 data class WeatherUiState(val sky: String = "", val temperature: String = "", val humidity: String = "")
