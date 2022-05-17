57d0a48cd2e463c32327f0209a0c43d253496f27
luis hasbon <lhaxre89@gmail.com>
2022-05-17 19:45:53 +0200

    Add basic geocoding with postal code or address

--- app/src/main/java/com/example/weatherbase/MainViewModel.kt
+++ app/src/main/java/com/example/weatherbase/MainViewModel.kt
@@ -15,13 +15,13 @@ class MainViewModel : ViewModel() {
     private val mutableState = MutableStateFlow(WeatherUiState())
 
     suspend fun onPostalCodeInput(code: String) = withContext(Dispatchers.IO) {
-        "https://dark-sky.p.rapidapi.com/40.461564,-3.636349"
-            .httpGet()
+        "https://forward-reverse-geocoding.p.rapidapi.com/v1/search"
+            .httpGet(listOf("q" to code))
             .header(
-                "X-RapidAPI-Host" to "dark-sky.p.rapidapi.com",
+                "X-RapidAPI-Host" to "forward-reverse-geocoding.p.rapidapi.com",
                 "X-RapidAPI-Key" to "718390cf80mshbaae151ab7aa8acp1df702jsnac8cc8f5308b"
             )
-            .responseString {_, _, result ->
+            .responseString { _, _, result ->
                 mutableState.value = WeatherUiState(result.get(), "", "")
             }
     }
